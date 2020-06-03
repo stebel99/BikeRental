c6700e931e525ec2d3cc31ed966eb39a47f9c4cb
Danil Lebets <stebel99@gmail.com>
2020-06-03 14:37:11 +0300

    Add navigation fields

--- Models/Status.cs
+++ Models/Status.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace BikeRental.API.Models
 {
     public class Status
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<Bike> Bikes { get; set; }
     }
 }
--- Models/Type.cs
+++ Models/Type.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace BikeRental.API.Models
 {
     public class Type
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<Bike> Bikes { get; set; }
     }
 }
