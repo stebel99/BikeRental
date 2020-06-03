e84468a93c9dd31167fbbb14664aa197703c8fd5
Danil Lebets <stebel99@gmail.com>
2020-06-03 15:40:45 +0300

    Add initial data for db

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,9 +1,35 @@
+using BikeRental.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BikeRental.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DbSet<Bike> Bikes { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Type> Types { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Status>().HasData(
+                new Status[]
+                {
+                    new Status { Id = 1, Name = "Available"},
+                    new Status { Id = 2, Name = "Rented" }
+                });
+
+            builder.Entity<Type>().HasData(
+                new Type[]
+                {
+                    new Type {Id = 1, Name = "Road" },
+                    new Type {Id = 2, Name = "Mountain" },
+                    new Type {Id = 3, Name = "Common" },
+                });
+        }
     }
 }
