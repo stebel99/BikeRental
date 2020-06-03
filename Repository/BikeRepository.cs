035931e338d77c97a6eed8a8ff14a4692b8a0d49
Danil Lebets <stebel99@gmail.com>
2020-06-03 18:20:05 +0300

    Add bike repository

---
+++ Repository/BikeRepository.cs
@@ -0,0 +1,72 @@
+﻿using BikeRental.API.Data;
+using BikeRental.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeRental.API.Repository
+{
+    public class BikeRepository : IBikeRepository, IDisposable
+    {
+        private readonly DataContext _context;
+
+        public BikeRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void DeleteBike(Guid bikeId)
+        {
+            Bike bike = _context.Bikes.Find(bikeId);
+            _context.Bikes.Remove(bike);
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public Bike GetBikeById(Guid bikeId)
+        {
+            return _context.Bikes.Find(bikeId);
+        }
+
+        public IEnumerable<Bike> GetBikes()
+        {
+            return _context.Bikes.ToList();
+        }
+
+        public void InsertBike(Bike bike)
+        {
+            _context.Bikes.Add(bike);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void UpdateBike(Bike bike)
+        {
+            _context.Entry(bike).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Repository/IBikeRepository.cs
@@ -0,0 +1,16 @@
+﻿using BikeRental.API.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BikeRental.API.Repository
+{
+    public interface IBikeRepository : IDisposable
+    {
+        IEnumerable<Bike> GetBikes();
+        Bike GetBikeById(Guid bikeId);
+        void InsertBike(Bike bike);
+        void DeleteBike(Guid bikeId);
+        void UpdateBike(Bike bike);
+        void Save();
+    }
+}
