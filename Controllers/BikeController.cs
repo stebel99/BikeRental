777fd3f66b6ab7bd90d872d07ff73990923330a7
Danil Lebets <stebel99@gmail.com>
2020-06-03 16:44:51 +0300

    Create deafault controller

---
+++ Controllers/BikeController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BikeRental.API.Data;
+using BikeRental.API.Models;
+
+namespace BikeRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BikeController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BikeController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bike
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bike>>> GetBikes()
+        {
+            return await _context.Bikes.ToListAsync();
+        }
+
+        // GET: api/Bike/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bike>> GetBike(Guid id)
+        {
+            var bike = await _context.Bikes.FindAsync(id);
+
+            if (bike == null)
+            {
+                return NotFound();
+            }
+
+            return bike;
+        }
+
+        // PUT: api/Bike/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBike(Guid id, Bike bike)
+        {
+            if (id != bike.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bike).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BikeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bike
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Bike>> PostBike(Bike bike)
+        {
+            _context.Bikes.Add(bike);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBike", new { id = bike.Id }, bike);
+        }
+
+        // DELETE: api/Bike/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Bike>> DeleteBike(Guid id)
+        {
+            var bike = await _context.Bikes.FindAsync(id);
+            if (bike == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bikes.Remove(bike);
+            await _context.SaveChangesAsync();
+
+            return bike;
+        }
+
+        private bool BikeExists(Guid id)
+        {
+            return _context.Bikes.Any(e => e.Id == id);
+        }
+    }
+}
