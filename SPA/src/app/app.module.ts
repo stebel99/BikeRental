f36ee0d88673c73f36428b8fb21c2d88c3a60cca
Danil Lebets <stebel99@gmail.com>
2020-06-05 01:09:47 +0300

    Generate bike component

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BikeComponent } from './bike/bike.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikeComponent
   ],
   imports: [
     BrowserModule,
