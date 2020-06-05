2bc9e916862b15ef8c7ee7ec896bb17da66799ac
Danil Lebets <stebel99@gmail.com>
2020-06-05 04:29:19 +0300

    Add interface of bike

---
+++ SPA/src/app/Interfaces/Bike.ts
@@ -0,0 +1,13 @@
+export class Bike {
+  constructor(
+    public id?: string,
+    public name?: string,
+    public price?: number,
+
+    public statusId?: number,
+    public statusName?: string,
+
+    public typeId?: number,
+    public typeName?: string
+  ) { }
+}
