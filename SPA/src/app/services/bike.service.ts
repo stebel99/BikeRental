bfd30b6c3510724d74054efd7b06fd3cfe7d50bd
Danil Lebets <stebel99@gmail.com>
2020-06-05 14:09:08 +0300

    Add bike service

---
+++ SPA/src/app/services/bike.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Bike } from '../interfaces/bike';
+
+@Injectable()
+export class BikeService {
+
+  private url = "/api/bike";
+
+  constructor(private http: HttpClient) {
+  }
+
+  getBikes() {
+    return this.http.get(this.url);
+  }
+
+  getBike(id: string) {
+    return this.http.get(this.url + '/' + id);
+  }
+
+  createBike(bike: Bike) {
+    return this.http.post(this.url, bike);
+  }
+
+  updateBike(bike: Bike, id: string) {
+    return this.http.put(this.url + '/' + id, bike);
+  }
+
+  deleteBike(id: string) {
+    return this.http.delete(this.url + '/' + id);
+  }
+}
