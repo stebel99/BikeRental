a2769b6406026e2cdab40657b80b2631209a6981
Danil Lebets <stebel99@gmail.com>
2020-06-07 06:21:50 +0300

    Cleaning

--- Controllers/BikeController.cs
+++ Controllers/BikeController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BikeRental.API.Models;
 using BikeRental.API.Repository;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BikeRental.API.Controllers
 {
